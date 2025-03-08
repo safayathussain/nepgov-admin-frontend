8d92c112011f8828c257c9dbdf0e8a91d0f6bb53
safayathussain <safayatofficial1@gmail.com>
2025-03-09 02:14:38 +0600

    test

---
+++ middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+export function middleware(request) {
+  // Log the request path
+  console.log('Request path:', request.cookies.get("adminAccessToken"));
+localStorage.setItem('test', request.cookies.get("adminAccessToken").value)
+  // Example: Redirect unauthenticated users
+  const isAuthenticated = request.cookies.get('auth-token') !== undefined;
+
+  if (!isAuthenticated && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  // Continue with the request
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ['/crimes'],
+};
